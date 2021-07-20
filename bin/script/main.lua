5312b6d354ab65d19722de8dd47eec27493a375a
brinkqiang <brink.qiang@gmail.com>
2021-07-20 20:16:00 +0800

    add

--- bin/script/main.lua
+++ bin/script/main.lua
@@ -1,7 +1,7 @@
 -- lua script
-_ENV = require("luaftpserver")
+local luaftpserver = require("luaftpserver")
 
-local ftpserver = ftpserver.new("0.0.0.0", 2121)
+local ftpserver = luaftpserver.ftpserver.new("0.0.0.0", 2121)
 
 ftpserver:addUserAnonymous("anonymous", 511)
 ftpserver:addUser("user",   "pass", "root", 511)
